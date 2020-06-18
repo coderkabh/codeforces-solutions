380897fb19bd6e5259ab04a58058aee221a8e34d
Kanishk Bharadwaj <kanishkbharadwaj333@gmail.com>
2020-06-18 21:42:26 +0530

    Create 71A-Way Too Long Words.py

---
+++ A/71A-Way Too Long Words.py
@@ -0,0 +1,12 @@
+    n = int(input())
+     
+     
+    for i in range(n):
+        s = input()
+        if len(s) > 10:
+            print(s[0],end="")
+            print(len(s)-2,end="")
+            print(s[len(s)-1],end="")
+            print()
+        else:
+            print(s)
